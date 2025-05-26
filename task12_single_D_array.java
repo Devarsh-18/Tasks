f356ed866fb179f2665d8c0c54b098732d62c200
Devarsh-18 <125034028+Devarsh-18@users.noreply.github.com>
2025-05-26 12:30:18 +0530

    Delete task12_single_D_array.java

--- task12_single_D_array.java
+++
@@ -1,9 +0,0 @@
-package test_script.test_script;
-
-public class single_D_array {
-
-    public static void main(String[] args) {
-int[] no = new int[5];
- no = new int[]{1,2,3,4,5};
- System.out.println(no[1]);
-    }}
